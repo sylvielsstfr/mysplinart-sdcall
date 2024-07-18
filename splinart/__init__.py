e3fb2c2c310244d5626616db45cba9d79afbe91e
sylvielsstfr <dagoret@lal.in2p3.fr>
2024-07-18 11:34:34 +0200

    add all files

--- splinart/__init__.py
+++ splinart/__init__.py
@@ -3,8 +3,10 @@
 #
 # License: BSD 3 clause
 """Splinart package."""
+
 from .shapes import circle, line
 from .compute import update_img
 from .draw import save_img, show_img
+from .version import __version__
 
-__all__ = ["circle", "line", "update_img", "save_img", "show_img"]
+__all__ = ["circle", "line", "update_img", "save_img", "show_img", "__version__"]
